1568bf9be75333e5500028603ae9db1656e45371
HeikalE <eslam.heikal.ext@aptar.com>
2022-03-07 21:50:28 +0200

    update cors

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const cors = require('cors');
-app.use(cors());
 
 app.use(function (req, res, next) {
-    //res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     next();
 });
 
