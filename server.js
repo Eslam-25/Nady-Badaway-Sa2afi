3c14124b40c5d2335f58220a27e630c4836b0279
HeikalE <eslam.heikal.ext@aptar.com>
2022-03-08 00:28:05 +0200

    update server file

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    next();
-});
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/' }),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
