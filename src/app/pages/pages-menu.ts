8b3bcbc464348d199a4ccbd44aeddc097f341856
Eslam-25 <72421399+Eslam-25@users.noreply.github.com>
2022-04-10 15:21:03 +0200

    Update pages-menu.ts

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -9,7 +9,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'تسجيل المتسابقين',
     icon: 'people-outline',
-    link: '/pages/register-reader',
+    link: '/pages/register-reader/none',
   },
   {
     title: 'دخول',
