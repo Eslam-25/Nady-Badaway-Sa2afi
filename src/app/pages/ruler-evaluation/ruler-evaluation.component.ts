3a6760e147992bf2acb49ec00f4fd3e3675558be
Eslam-25 <72421399+Eslam-25@users.noreply.github.com>
2022-04-11 16:31:26 +0200

    Update ruler-evaluation.component.ts

--- src/app/pages/ruler-evaluation/ruler-evaluation.component.ts
+++ src/app/pages/ruler-evaluation/ruler-evaluation.component.ts
@@ -24,6 +24,7 @@ export class RulerEvaluationComponent implements OnInit {
   readerModel: ReaderModel;
   userIsReader: boolean = false;
   evaluationTextPLaceHolder: string = "اكتب هنا ...";
+  levelOfDegree = "فى انتظار التقييم !!";
 
   constructor(
     private readerService: ReaderSevice,
@@ -79,6 +80,18 @@ export class RulerEvaluationComponent implements OnInit {
       this.evaluationValue = reader.degree == 0 ? null : reader.degree;
       this.age = new Date().getFullYear() - new Date(this.readerModel?.birthDate).getFullYear();
       this.hasaReader = true;
+
+      if(reader.degree > 0 && reader.degree < 50)
+        this.levelOfDegree = "ضعيف";
+      else if(reader.degree >= 50 && reader.degree < 65)
+        this.levelOfDegree = "متوسط";
+      else if(reader.degree >= 65 && reader.degree < 75)
+        this.levelOfDegree = "جيد";
+      else if(reader.degree >= 75 && reader.degree < 90)
+        this.levelOfDegree = "جيد جدا";
+      else if(reader.degree >= 90 && reader.degree < 100)
+        this.levelOfDegree = "ممتاز";
+
     } else {
       this.toastService.showError("عفوا هذا المتسابق غير موجود")
       this.readerCode = null;
